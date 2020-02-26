1ef53c7c8df7deba64fbeeb1d276d379277d29f3
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-26 22:28:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,35 +12,8 @@ def add_element_to_end_of_array(array, element) = create_an_array
 end
 end
 
-def add_element_to_start_of_array(array, element) = create_an_array
-  add_element_to_start_of_array(array, element).unshift
-  puts add_element_to_start_of_array(array, element).inspect
-end
-end
 
-def remove_element_from_end_of_array(array) = create_an_array
-  Blue = remove_element_from_end_of_array(array).pop
-  puts remove_element_from_end_of_array(array).inspect
-end
-end
 
-def remove_element_from_start_of_array(array) = create_an_array
-  David = remove_element_from_start_of_array(array).shift 
-  puts remove_element_from_start_of_array(array).inspect
-end
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
 
 
 
