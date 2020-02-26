a12188d4eb30d5ced5b746c2fbeb0f7b48c7641e
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-26 22:34:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array,element)
-  array = ["you","us"]
-  array  << "David"
-  puts array.inspect
-end
+  array  << element
+ end
 end
 
 
